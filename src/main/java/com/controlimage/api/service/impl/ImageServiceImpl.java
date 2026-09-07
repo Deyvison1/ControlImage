144c74e23bbb6d7824731bcc918fcab1f3ff1a59
DESKTOP-PNMASB0\computador <tutacf@gmail.com>
2026-09-07 03:59:35 -0300

    asdasda

--- src/main/java/com/controlimage/api/service/impl/ImageServiceImpl.java
+++ src/main/java/com/controlimage/api/service/impl/ImageServiceImpl.java
@@ -17,12 +17,16 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 @Slf4j
 @Service
@@ -37,6 +41,7 @@ public class ImageServiceImpl implements IImageService {
 	private final IImageRepository repository;
 	private final IImageMapper mapper;
 	private final S3Client s3Client;
+	private final S3Presigner s3Presigner;
 
 	@Override
 	@Transactional
@@ -104,7 +109,19 @@ public class ImageServiceImpl implements IImageService {
 
 		Image image = findById(id);
 
-		return String.format("%s/%s/%s", publicUrl, image.getBucket(), image.getObjectKey());
+		GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+				.bucket(image.getBucket())
+				.key(image.getObjectKey())
+				.build();
+
+		GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+				.signatureDuration(Duration.ofHours(1))
+				.getObjectRequest(getObjectRequest)
+				.build();
+
+		return s3Presigner.presignGetObject(presignRequest)
+				.url()
+				.toString();
 	}
 
 	@Override
